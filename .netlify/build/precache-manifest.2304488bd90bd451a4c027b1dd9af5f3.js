2cbaf2dd4a50dff1384742ff7af2be99b0c8bde5
dimachello <korovind195@gmail.com>
2019-12-22 17:28:51 +0200

    confirmed

---
+++ .netlify/build/precache-manifest.2304488bd90bd451a4c027b1dd9af5f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6ef8740540ad0c54fb722141adb53c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dc90c32b04f904ad980",
+    "url": "/static/css/main.cb6a0f82.chunk.css"
+  },
+  {
+    "revision": "4d7da97ff8b143fbd983",
+    "url": "/static/js/2.551a6a32.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.551a6a32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6dc90c32b04f904ad980",
+    "url": "/static/js/main.cce7a7ff.chunk.js"
+  },
+  {
+    "revision": "a2250df6194528c1b1c9",
+    "url": "/static/js/runtime-main.d67e7bc4.js"
+  },
+  {
+    "revision": "dcc389e54c4374fb8ccf2e8fc2ecedf9",
+    "url": "/static/media/logo-print.dcc389e5.png"
+  }
+]);
