5e1b9d82fa21e96c9c5999ca31fa7a154d2d9a0d
dimachello <korovind195@gmail.com>
2019-12-27 21:16:44 +0200

    explore section added

--- src/components/main-content/search/search-list-item/search-list-item.js
+++ src/components/main-content/search/search-list-item/search-list-item.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./search-list-item.css";
 import like from "../../../../imgs/icons/like24.png";
 import comment from "../../../../imgs/icons/commnet24.png";
+import { LazyLoadComponent } from 'react-lazy-load-image-component';
 
 class SearchListItem extends React.Component {
   render() {
@@ -9,8 +10,10 @@ class SearchListItem extends React.Component {
       <li className="search-item">
         <img src={this.props.url} alt="search-item" />
         <p className="author">
+            <LazyLoadComponent>
             <img src={like} alt="item like" />
             <img src={comment} alt="comment" />
+            </LazyLoadComponent>
             <span>{this.props.name}</span>
         </p>
       </li>
